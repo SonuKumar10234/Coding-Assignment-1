06c36b879ea5772b166372b18021f68f3c6bb593
Monukr9236 <90559872+Monukr9236@users.noreply.github.com>
2023-02-18 11:02:56 +0530

    coding Assignment

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import logo from './logo.svg';
+import cafedata from './JsonFile/cafedata.json';
+import placedata from './JsonFile/placedata.json';
 import './App.css';
 
+
 function App() {
+
+  //Question 1:  Implementation 
+  function findCaliforniaCafes(str) {
+    // You can store the given arrays in 2 internal variables
+    var cafes = cafedata;
+    var places = placedata;
+
+    // Your code goes here
+    var ans = [];
+    var cafeName = [];
+    var placeId = [];
+
+    
+    cafes.map((s) =>{
+       var p = s.name;
+      if(p.match(str)){
+          cafeName.push(p);
+          placeId.push(s.place_id)
+      }
+    });
+    
+    // console.log(cafeName);
+    // console.log(placeId);
+    places.map((item)=>{
+       for (const i in placeId) {
+          if(item.id === placeId[i]){
+              var obj = {};
+              obj.name = cafeName[i];
+              obj.street_no = item.street_no;
+              obj.locality = item.locality;
+              obj.postal_code = item.postal_code;
+              obj.lat = item.lat;
+              obj.long = item.long;
+              
+              ans.push(obj);
+          }
+       }
+    });
+
+    return ans;
+  }
+  
+  const newArr = findCaliforniaCafes("Avenue"); // search cafes here
+  // console.log(newArr);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      
+      {
+        newArr.length > 0 ? newArr.map((item,i)=>
+          <div key={i} className="placeContainer">
+            <h3>Place {i+1}</h3>
+            <p>"name":"{item.name}"</p>
+            <p>"street_no":"{item.street_no}"</p>
+            <p>"locality":"{item.locality}"</p>
+            <p>"postal_code":"{item.postal_code}"</p>
+            <p>"lat":"{item.lat}"</p>
+            <p>"long":"{item.long}"</p>
+            <hr/>
+          </div>
+        )
+        :
+        <div>
+          <p>Sorry!, No Data are available at the moment.</p>
+        </div>
+      }
+
+    </div> 
   );
 }
 
